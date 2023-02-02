8925fc5f5ba7f38feff91d59602bbccf6b71e378
Ariadna Frias <ariadna.812@gmail.com>
2023-02-02 22:50:09 +0100

    Update index ts file

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 /* eslint-disable no-unused-vars */
 import { Header } from './components/header/header';
 import { Main } from './components/main/main';
-import { SeriesPending } from './seriesPending/seriesPending';
+import { SeriesPending } from './components/seriesPending/seriesPending';
 
 new Header('.container');
 new Main('header');
